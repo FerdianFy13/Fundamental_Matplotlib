8d977e359be822e7952d8fdebbf0bc057f76d668
FerdianFy13 <ferdianfy13@gmail.com>
2022-06-26 15:52:30 +0700

    update fundamental start

--- fundamental.py
+++ fundamental.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 
 a = np.array([[1, 2], [3, 4]])
 b = np.array([[5, 6], [7, 8]])
+c = np.array([[11, 5], [12, 14]])
 
-plt.plot(a, b)
+plt.plot(a, b, c)
 plt.show()
+
+# tutorial penggunaan matplotlib adalah
+# 1. membuat sebuah data
+# 2. membuat sebuah plot
+# 3. menampilkan sebuah plot
